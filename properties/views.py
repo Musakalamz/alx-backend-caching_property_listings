050cb0a70e0391393ce74898798543205341f20e
Musakalamz <ogunsolumusa@gmail.com>
2026-01-11 08:41:43 +0100

    Fix Task 0 & 1: Update docker-compose and property_list view

--- properties/views.py
+++ properties/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.views.decorators.cache import cache_page
+from django.http import JsonResponse
 from .models import Property
 
 @cache_page(60 * 15)
 def property_list(request):
     properties = Property.objects.all()
-    return render(request, 'properties/property_list.html', {'properties': properties})
+    data = list(properties.values('title', 'description', 'price', 'location', 'created_at'))
+    return JsonResponse({'data': data})
