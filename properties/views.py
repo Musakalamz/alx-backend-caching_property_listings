35b7b6d4473732e605ef8da529a02b115077b160
Musakalamz <ogunsolumusa@gmail.com>
2026-01-11 08:44:08 +0100

    Task 2: Low-Level Caching for Property Queryset

--- properties/views.py
+++ properties/views.py
@@ -2,9 +2,10 @@ from django.shortcuts import render
 from django.views.decorators.cache import cache_page
 from django.http import JsonResponse
 from .models import Property
+from .utils import get_all_properties
 
 @cache_page(60 * 15)
 def property_list(request):
-    properties = Property.objects.all()
+    properties = get_all_properties()
     data = list(properties.values('title', 'description', 'price', 'location', 'created_at'))
     return JsonResponse({'data': data})
